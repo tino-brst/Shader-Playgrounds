f957184a74ff1781abe1a22fb2ce4ec5a73e8b12
AgustinBrst <agustin.burset@icloud.com>
2019-06-13 10:27:56 -0300

    standard base setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,35 +3,21 @@ module.exports = {
 	env: {
 		node: true
 	},
-	"extends": [
-		"plugin:vue/recommended",
-		"@vue/standard",
-		"@vue/typescript"
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+		project: "./tsconfig.json"
+	},
+	extends: [
+		"plugin:vue/strongly-recommended",
+		"standard"
 	],
 	rules: {
-		"no-console": "off",
-		"no-debugger": "off",
-		"no-useless-escape": "off",
-		"space-in-parens": [ "error", "always" ],
-		"quotes": [ "error", "double" ],
-		"indent": [ "error", 4, { "SwitchCase": 1 } ],
-		"space-before-function-paren": [ "error", "never" ],
-		"space-unary-ops": [ 2, { "words": true, "nonwords": true } ],
-		"no-multi-spaces": "off",
-		"template-curly-spacing": [ "error", "always" ],
-		// "padded-blocks": [ "error", "always" ],
+		// Replace eslint rules with Typescript aware equivalents (when available)
+		"no-unused-vars": "off",
 		"no-useless-constructor": "off",
-		"import/export": "off",
-		"array-bracket-spacing": [ "error", "always" ],
-		"arrow-body-style": [ "error", "as-needed" ],
-		"arrow-parens": [ "error", "as-needed", { "requireForBlockBody": true } ],
-        "arrow-spacing": "error",
-        "semi": [ "error", "never" ],
-		"vue/html-indent": [ "error", 4 ],
-		"vue/max-attributes-per-line": false,
-		"vue/attributes-order": false
-	},
-	parserOptions: {
-		parser: "@typescript-eslint/parser"
-    }
+		"no-undef": "off",
+		"@typescript-eslint/no-unused-vars": "error",
+		"@typescript-eslint/no-useless-constructor": "error",
+	}
 }
